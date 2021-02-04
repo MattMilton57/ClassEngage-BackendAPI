53830407602773fac4e0243fb6132bd0cf39abb6
github username <github email address>
2021-02-03 16:01:14 -0800

    adding custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
       post '/class_periods', to: 'class_periods#create'
       post '/filterCP', to: 'class_periods#filter'
       post '/filterR', to: 'registrations#filter' 
-      get 'profile', to: 'users#profile'
       post '/classList', to: 'class_periods#students'
       post '/classAssessments', to: 'assessments#classAssessments'
       post '/studentAssessments', to: 'assessments#studentAssessments'
+
+      get 'profile', to: 'users#profile'
+      get '/current_user', to: 'auth#show'
+      get '/current_student/:id',  to: 'students#show'
     end
   end
 end
